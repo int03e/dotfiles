f566a81f3d9829b4e830fcd80a85b1953a42f371
int03e <ecuezzo@gmail.com>
2026-05-03 21:21:49 +0200

    hey

--- nixos/modules/boot-hardware.nix
+++ nixos/modules/boot-hardware.nix
@@ -1,25 +1,21 @@
-{ pkgs, lib, ... }:
+{ pkgs, ... }:
 
 {
   boot = {
     kernelPackages = pkgs.linuxPackages_latest;
-
     kernelModules = [ ];
-
     blacklistedKernelModules = [
       "asus_wmi"
       "asus_nb_wmi"
       "uniwill_wmi"
       "clevo_wmi"
     ];
-
     extraModprobeConfig = ''
       install asus_wmi /run/current-system/sw/bin/false
       install asus_nb_wmi /run/current-system/sw/bin/false
       install uniwill_wmi /run/current-system/sw/bin/false
       install clevo_wmi /run/current-system/sw/bin/false
     '';
-
     kernelParams = [
       "acpi_backlight=native"
       "amdgpu.backlight=0"
@@ -28,20 +24,14 @@
       "resume_offset=239663104"
       "rtc_cmos.use_acpi_alarm=1"
     ];
-
     resumeDevice = "/dev/disk/by-uuid/2a88ba25-c2ba-48c0-988a-09009ea1efdd";
-
     loader.grub = {
       enable = true;
       device = "nodev";
       efiSupport = true;
       useOSProber = true;
-
-      gfxmodeEfi = "auto";
-
-      font = "${pkgs.grub2}/share/grub/unicode.pf2";
-
-      fontSize = 64;
+      gfxmodeEfi = "2880x1800";
+      theme = pkgs.sleek-grub-theme;
     };
   };
 
