3ed40596ac78ccf3a2308f64aea7ff5e5a8ef48d
int03e <ecuezzo@gmail.com>
2026-08-24 12:04:17 +0200

    dotfiles

---
+++ nixos/files/config/fish/functions/dotfiles.fish
@@ -0,0 +1,3 @@
+function dotfiles --wraps='cd /home/int03e/projects/dotfiles/nixos/files/config' --description 'alias dotfiles=cd /home/int03e/projects/dotfiles/nixos/files/config'
+    cd /home/int03e/projects/dotfiles/nixos/files/config $argv
+end
